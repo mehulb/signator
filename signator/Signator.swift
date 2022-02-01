fda0824ebe360c3d89378458510d0335e4ab26fb
mehulb <mehulb@live.com>
2022-02-02 00:23:41 +0530

    Added CommandCenter

--- signator/Signator.swift
+++ signator/Signator.swift
@@ -8,56 +8,36 @@
 import Foundation
 
 enum SignatorError: Error {
-    case SignErr(_ errMsg: String)
+    case InvalidFilePath
+    case UpdateBlockMissing
+    case CompletionBlockMissing
+    case msg(_ : String)
 }
 
 class Signator {
-//    private init() {}
-//    static let shared = Signator()
-    
     typealias CodeBlock = (String?, String?, Error?) -> Void
     
-    private var _path: String?
-    private var _updateBlock: CodeBlock?
-    private var _completionBlock: CodeBlock?
+    private var path: String
+    private var updateBlock: CodeBlock
+    private var completionBlock: CodeBlock
     
-    func sign(path: String, update: @escaping CodeBlock, completion: @escaping CodeBlock) {
-        _path = path
-        _updateBlock = update
-        _completionBlock = completion
-        
-        if let _updateBlock = _updateBlock {
-            _updateBlock("Filepath", "\(path)\n", nil)
-        }
-        
-        checkSPCTL()
-        checkXATTR()
-        checkCODESIGN()
+    init(withPath path: String, update: @escaping CodeBlock, completion: @escaping CodeBlock) {
+        self.path = path
+        self.updateBlock = update
+        self.completionBlock = completion
     }
-    
-    private func checkSPCTL() {
-        if let path = _path, let update = _updateBlock {
-            let result = runShellCommand(withLaunchPath: "/usr/sbin/spctl", arguments: ["-av", "\(path)"])
-            update("spctl -av", result.message, nil)
+    func sign() {
+        for cmd in CommandCenter.shared.commands {
+            let result = runShellCommand(withLaunchPath: cmd.path, arguments: [cmd.arguments, "\(path)"])
+            updateBlock(cmd.title, result.message, nil)
         }
-    }
-    private func checkXATTR() {
-        if let path = _path, let update = _updateBlock {
-            let result = runShellCommand(withLaunchPath: "/usr/bin/xattr", arguments: ["\(path)"])
-            update("xattr", result.message, nil)
-        }
-    }
-    private func checkCODESIGN() {
-        if let path = _path, let update = _updateBlock {
-            let result = runShellCommand(withLaunchPath: "/usr/bin/codesign", arguments: ["-dvvvv", "\(path)"])
-            update("codesign -dvvvv", result.message, nil)
-        }
+        completionBlock(nil, nil, nil)
     }
 }
 
 extension Signator {
     private func runShellCommand(withLaunchPath launchPath: String, arguments: [String], timeout: TimeInterval = 0) -> (status: Bool, message: String) {
-        print("CMD: \(launchPath) \(arguments)")
+        Logger.info("CMD: \(launchPath) \(arguments)")
         
         var _status = true
         var _message = ""
@@ -91,7 +71,7 @@ extension Signator {
             _message = msg
         }
         
-        print("STS: \(_status)\nMSG: \(_message)")
+        Logger.info("STS: \(_status)\nMSG: \(_message)")
         return (_status, _message)
     }
 }
@@ -100,7 +80,7 @@ extension Process {
     func waitUntil(futureDate: Date) {
         while self.isRunning {
             if Date() > futureDate {
-                print("ERR: task taking too long, terminate!")
+                Logger.info("ERR: task taking too long, terminate!")
                 self.terminate()
             }
             Thread.sleep(forTimeInterval: 1.0)
